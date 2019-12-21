0ae74431bb21c4da4f5c712310500a8251d33ff1
Dave Kane <davelkane@gmail.com>
2019-12-21 14:26:11 -0600

    Removed morgan import again

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const morgan = require('morgan')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router')
@@ -13,7 +12,6 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-// server.use(morgan("dev"))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', authMiddleware, usersRouter)
